a3f41422cdfd10ccf0cf4b9920dcf40225354dc3
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2024-10-22 23:58:27 -0400

    add api prefix

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
+import { Logger } from '@nestjs/common';
+import { apiPrefix } from './constants';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(apiPrefix);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
